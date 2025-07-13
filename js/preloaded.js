6071c00050f1ba172ddc2dc52eab88754d846e8e
Sevmamra <sevmamra01@gmail.com>
2025-07-13 15:44:09 +0530

    Delete js directory

--- js/preloaded.js
+++
@@ -1,6 +0,0 @@
-// Preloader hide after page fully loads
-window.addEventListener("load", function () {
-    const preloader = document.getElementById("preloader");
-    preloader.style.opacity = "0";
-    preloader.style.visibility = "hidden";
-});
